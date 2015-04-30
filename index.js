23829d320324d492a1de90146d8b00ee6c6f4c69
Timon Reinhard <hallo@timonreinhard.de>
2015-04-30 17:42:55 +0200

    Discover wemo bridge in local network

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ var Scout = require('zetta-scout');
 var util = require('util');
 var Wemo = require('./wemo');
 
+var SSDP = require('node-ssdp').Client;
+var request = require('request');
+var xml2js = require('xml2js');
+var url = require('url');
+var http = require('http');
+var util = require('util');
+
 var WemoScout = module.exports = function() {
   Scout.call(this);
 };
@@ -11,8 +18,14 @@ WemoScout.prototype.init = function(next) {
 
   var self = this;
 
-  var query = this.server.where({type: 'wemo'});
-  var options = {default: 'DEFAULT'};
+  this.discover();
+
+  var query = this.server.where({
+    type: 'wemo'
+  });
+  var options = {
+    default: 'DEFAULT'
+  };
 
   this.server.find(query, function(err, results) {
     if (results[0]) {
@@ -24,4 +37,71 @@ WemoScout.prototype.init = function(next) {
 
   next();
 
+};
+
+WemoScout.prototype.soapAction = function(device, path, action, body) {
+  var header = ['<?xml version="1.0" encoding="utf-8"?>',
+    '<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">',
+    '<s:Body>'].join('\n');
+  var footer = ['</s:Body>','</s:Envelope>'].join('\n');
+
+  var post = http.request({
+    host: device.ip,
+    port: device.port,
+    path: path,
+    method: 'POST',
+    headers: {
+      SOAPACTION: '"' + action + '"',
+      'Content-Type': 'text/xml; charset="utf-8"',
+      Accept: ''
+    }
+  }, function(res) {
+    var data = '';
+    res.setEncoding('utf8');
+    res.on('data', function(chunk) {
+      data += chunk;
+    });
+    res.on('end', function() {
+      console.log(data);
+    });
+  });
+  post.write(header);
+  post.write(body);
+  post.write(footer);
+	post.end();
+};
+
+WemoScout.prototype.discover = function() {
+  var self = this;
+
+  var handleUDPResponse = function (msg, statusCode, rinfo) {
+    if (msg.ST === 'urn:Belkin:service:bridge:1') {
+      request.get(msg.LOCATION, function(err, res, xml) {
+				if (!err) {
+					xml2js.parseString(xml, function(err, json) {
+						if (!err) {
+              var location = url.parse(msg.LOCATION);
+              var device = { ip: location.hostname, port: location.port };
+							for (var key in json.root.device[0]) {
+								device[key] = json.root.device[0][key][0];
+							}
+              self.foundDevice(device);
+            }
+          });
+        }
+      });
+    }
+  }
+
+  var client = new SSDP();
+  client.on('response', handleUDPResponse);
+  client.search('ssdp:all');
+};
+
+WemoScout.prototype.foundDevice = function(device) {
+  console.log('found device: ', device);
+  this.soapAction(device, '/upnp/control/bridge1',
+    'urn:Belkin:service:bridge:1#GetEndDevices',
+    '<u:GetEndDevices xmlns:u="urn:Belkin:service:bridge:1"><DevUDN>' + device.UDN + '</DevUDN><ReqListType>PAIRED_LIST</ReqListType></u:GetEndDevices>'
+  );
 };
